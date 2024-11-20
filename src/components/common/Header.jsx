b7275bcdb4011f8399156367e511420538cb21bb
SeohyunSim <angelsh0805@gachon.ac.kr>
2024-11-20 16:40:00 +0900

    모바일, 태블릿 호환 삭제

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import useDevice from "../../hooks/useDevice";
-import MenuIcon from "../../assets/icons/common/menuIcon.svg?react";
-import CancelIcon from "../../assets/icons/common/cancelIcon.svg?react";
 import LogoIcon from "../../assets/icons/common/logo.svg?react";
 
 function Header() {
@@ -19,20 +16,10 @@ function Header() {
         setIsVisibleMenu(false);
     };
 
-    // 반응형 함수
-    const { isDesktop, isTablet, isMobile } = useDevice();
-
     // 메뉴 열고 닫기
     const handleMenu = () => {
         setIsVisibleMenu(!isVisibleMenu);
     }
-    
-    // 화면 크기가 변경 시 메뉴를 닫기
-    useEffect(() => {
-        if (!isMobile) {
-            setIsVisibleMenu(false);
-        }
-    }, [isMobile, isTablet, isDesktop]);;
     
     // 메뉴바 참조를 위한 ref
     const menuRef = useRef(null);
@@ -59,49 +46,14 @@ function Header() {
     return (
         <>
         <HeaderWrapper>
-            { isDesktop &&
-                <>
-                    <StyledLogoIcon onClick={() => handleNavigation("/")}/>
-                    <ButtonWrapper>
-                        <StyledButton onClick={() => handleNavigation("/")}>홈</StyledButton>
-                        <StyledButton onClick={() => handleNavigation("/board")}>게시판</StyledButton>
-                        <StyledButton onClick={() => handleNavigation("/mypage")}>마이페이지</StyledButton>
-                        <StyledButton onClick={() => handleNavigation("/login")}>로그인</StyledButton>
-                    </ButtonWrapper>
-                </>
-            }
-            { isTablet &&
-                <>
-                    <StyledLogoIcon onClick={() => handleNavigation("/")}/>
-                    <ButtonWrapper>
-                        <StyledButton onClick={() => handleNavigation("/")}>홈</StyledButton>
-                        <StyledButton onClick={() => handleNavigation("/board")}>게시판</StyledButton>
-                        <StyledButton onClick={() => handleNavigation("/mypage")}>마이페이지</StyledButton>
-                        <StyledButton onClick={() => handleNavigation("/login")}>로그인</StyledButton>
-                    </ButtonWrapper>
-                </>
-            }
-            { isMobile &&
-                <>
-                    <StyledLogoIcon onClick={() => handleNavigation("/")}/>
-                    <StyledMenuIcon onClick={() => handleMenu()}/>
-                </>
-            }
-            
+            <StyledLogoIcon onClick={() => handleNavigation("/")}/>
+            <ButtonWrapper>
+                <StyledButton onClick={() => handleNavigation("/")}>홈</StyledButton>
+                <StyledButton onClick={() => handleNavigation("/board")}>게시판</StyledButton>
+                <StyledButton onClick={() => handleNavigation("/mypage")}>마이페이지</StyledButton>
+                <StyledButton onClick={() => handleNavigation("/login")}>로그인</StyledButton>
+            </ButtonWrapper>
         </HeaderWrapper>
-        
-        { isMobile &&
-            // 메뉴바
-            <MenuWrapper isVisible={isVisibleMenu} ref={menuRef}>
-                <StyledCancelIcon onClick={() => handleMenu()}/>
-                <MobileButtonWrapper>
-                    <MobileButton onClick={() => handleNavigation("/")}>홈</MobileButton>
-                    <MobileButton onClick={() => handleNavigation("/board")}>게시판</MobileButton>
-                    <MobileButton onClick={() => handleNavigation("/mypage")}>마이페이지</MobileButton>
-                    <MobileButton onClick={() => handleNavigation("/login")}>로그인</MobileButton>
-                </MobileButtonWrapper>
-            </MenuWrapper>
-        }
         </>
     )
 }
@@ -122,25 +74,11 @@ const HeaderWrapper = styled.div`
     backdrop-filter: blur(15px);
     z-index: 2000;
     box-sizing: border-box;
-    
-    @media (max-width: 1024px) {  // 태블릿
-        padding-left: 2.5em;
-    }
-    @media (max-width: 768px) {  // 모바일
-        padding-left: 1em;
-    }
 `;
 
 const StyledLogoIcon = styled(LogoIcon)`
     height: 4.5em;
     cursor: pointer;
-
-    @media (max-width: 1024px) {  // 태블릿
-        height: 3.8em;
-    }
-    @media (max-width: 768px) {  // 모바일
-        height: 3em;
-    }
 `;
 
 const ButtonWrapper = styled.div`
@@ -156,57 +94,4 @@ const StyledButton = styled.div`
     font-size: 1.2em;
     font-weight: 600;
     cursor: pointer;
-
-     @media (max-width: 1024px) {  // 태블릿
-        font-size: 1em;
-    }
-`;
-
-// 모바일 반응형
-const StyledMenuIcon = styled(MenuIcon)`
-    width: 2em;
-    height: 2em;
-    cursor: pointer;
-`;
-
-const MenuWrapper = styled.div`
-    width: 15em;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 100;
-    display: flex;
-    flex-direction: column;
-    background-color: rgba(26,26,26,0.7);
-    backdrop-filter: blur(15px);
-    transform: ${({ isVisible }) => (isVisible ? 'translateX(0)' : 'translateX(100%)')};
-    transition: transform 0.3s ease;
-    z-index: 3000;
-`;
-
-const StyledCancelIcon = styled(CancelIcon)`
-    margin: 2.5em 1em 1em 1em;
-    width: 2.3em;
-    height: 2.3em;
-    cursor: pointer;
-`;
-
-const MobileButtonWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
-
-const MobileButton = styled.div`
-    margin-left: 3em;
-    margin-bottom: 1.5em;
-    color: white;
-    font-family: 'Pretendard-SemiBold';
-    font-size: 1.2em;
-    font-weight: 600;
-    cursor: pointer;
-    &:hover {
-        transform: translateX(5px);
-        }
-    transition: all 0.3s ease;
 `;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import useDevice from "../hooks/useDevice";
 import BackgroundImg from "../assets/images/loginPage/background2.png";
 import KakaoLogo from "../assets/icons/loginPage/kakaoLogo.svg?react";
 import GoogleLogo from "../assets/icons/loginPage/googleLogo.svg?react";
@@ -18,140 +17,48 @@ function LoginPage() {
         setIsCheck(!isCheck);
     }
 
-    // 반응형 함수
-    const { isDesktop, isTablet, isMobile } = useDevice();
-    
     return (
         <PageWrapper>
-            {/* PC 버전 */}
-            { isDesktop &&
-                <ContentWrapper>
-                    {/* 좌측 영역 */}
-                    <LeftWrapper>
-                        후기와 예약까지<br/>
-                        한 번에 살필 수 있는,
-                    </LeftWrapper>
-                    
-                    {/* 우측 영역 */}
-                    <RightWrapper>
-                        <UpWrapper>
-                            {/* 아이디 */}
-                            <InputWrapper>
-                                <InputText>
-                                    아이디
-                                </InputText>
-                                <StyledInput/>
-                            </InputWrapper>
-                            {/* 비밀번호 */}
-                            <InputWrapper>
-                                <InputText>
-                                    비밀번호
-                                </InputText>
-                                <StyledInput type="password"/>
-                            </InputWrapper>
-                            {/* 로그인상태유지 버튼 */}
-                            <LoginStateWrapper>
-                                    <StyledCheckbox onClick={handleCheckBox} isCheck={isCheck}>✓</StyledCheckbox>
-                                    <CheckboxText onClick={handleCheckBox}>로그인 상태 유지하기</CheckboxText>
-                            </LoginStateWrapper>
-                        </UpWrapper>
-                        <DownWrapper>
-                            <SocialLoginButton onClick={() => navigate("/")}><StyledKakaoLogo/>카카오로 시작하기</SocialLoginButton>
-                            <SocialLoginButton onClick={() => navigate("/")}><StyledGoogleLogo/>구글로 시작하기</SocialLoginButton>
-                            <LoginButton onClick={() => navigate("/")}>로그인</LoginButton>
-                            <SignupText onClick={() => navigate("/signup")}>
-                                회원가입 하기
-                            </SignupText>
-                        </DownWrapper>
-                    </RightWrapper>
-                </ContentWrapper>
-                }
-
-
-            {/* 태블릿 버전 */}
-            { isTablet &&
-                <TabletContentWrapper>
-                    {/* 좌측 영역 */}
-                    <TabletLeftWrapper>
-                        후기와 예약까지<br/>
-                        한 번에 살필 수 있는,
-                    </TabletLeftWrapper>
-                    
-                    {/* 우측 영역 */}
-                    <TabletRightWrapper>
-                        <UpWrapper>
-                            {/* 아이디 */}
-                            <InputWrapper>
-                                <InputText>
-                                    아이디
-                                </InputText>
-                                <TabletStyledInput/>
-                            </InputWrapper>
-                            {/* 비밀번호 */}
-                            <InputWrapper>
-                                <InputText>
-                                    비밀번호
-                                </InputText>
-                                <TabletStyledInput type="password"/>
-                            </InputWrapper>
-                            {/* 로그인상태유지 버튼 */}
-                            <LoginStateWrapper>
-                                    <StyledCheckbox onClick={handleCheckBox} isCheck={isCheck}>✓</StyledCheckbox>
-                                    <CheckboxText onClick={handleCheckBox}>로그인 상태 유지하기</CheckboxText>
-                            </LoginStateWrapper>
-                        </UpWrapper>
-                        <DownWrapper>
-                            <LoginButton onClick={() => navigate("/")}>로그인</LoginButton>
-                            <SignupText onClick={() => navigate("/signup")}>
-                                회원가입 하기
-                            </SignupText>
-                        </DownWrapper>
-                    </TabletRightWrapper>
-                </TabletContentWrapper>
-                }
-
-
-            {/* 모바일 버전 */}
-            { isMobile &&
-                <MobileContentWrapper>
-                    {/* 좌측 영역 */}
-                    <MobileLeftWrapper>
-                        후기와 예약까지<br/>
-                        한 번에 살필 수 있는,
-                    </MobileLeftWrapper>
-                    
-                    {/* 우측 영역 */}
-                    <MobileRightWrapper>
-                        <UpWrapper>
-                            {/* 아이디 */}
-                            <InputWrapper>
-                                <InputText>
-                                    아이디
-                                </InputText>
-                                <MobileStyledInput/>
-                            </InputWrapper>
-                            {/* 비밀번호 */}
-                            <InputWrapper>
-                                <InputText>
-                                    비밀번호
-                                </InputText>
-                                <MobileStyledInput type="password"/>
-                            </InputWrapper>
-                            {/* 로그인상태유지 버튼 */}
-                            <LoginStateWrapper>
-                                    <StyledCheckbox onClick={handleCheckBox} isCheck={isCheck}>✓</StyledCheckbox>
-                                    <CheckboxText onClick={handleCheckBox}>로그인 상태 유지하기</CheckboxText>
-                            </LoginStateWrapper>
-                        </UpWrapper>
-                        <DownWrapper>
-                            <MobileLoginButton onClick={() => navigate("/")}>로그인</MobileLoginButton>
-                            <SignupText onClick={() => navigate("/signup")}>
-                                회원가입 하기
-                            </SignupText>
-                        </DownWrapper>
-                    </MobileRightWrapper>
-                </MobileContentWrapper>
-                }
+            <ContentWrapper>
+                {/* 좌측 영역 */}
+                <LeftWrapper>
+                    후기와 예약까지<br/>
+                    한 번에 살필 수 있는,
+                </LeftWrapper>
+                
+                {/* 우측 영역 */}
+                <RightWrapper>
+                    <UpWrapper>
+                        {/* 아이디 */}
+                        <InputWrapper>
+                            <InputText>
+                                아이디
+                            </InputText>
+                            <StyledInput/>
+                        </InputWrapper>
+                        {/* 비밀번호 */}
+                        <InputWrapper>
+                            <InputText>
+                                비밀번호
+                            </InputText>
+                            <StyledInput type="password"/>
+                        </InputWrapper>
+                        {/* 로그인상태유지 버튼 */}
+                        <LoginStateWrapper>
+                                <StyledCheckbox onClick={handleCheckBox} isCheck={isCheck}>✓</StyledCheckbox>
+                                <CheckboxText onClick={handleCheckBox}>로그인 상태 유지하기</CheckboxText>
+                        </LoginStateWrapper>
+                    </UpWrapper>
+                    <DownWrapper>
+                        <SocialLoginButton onClick={() => navigate("/")}><StyledKakaoLogo/>카카오로 시작하기</SocialLoginButton>
+                        <SocialLoginButton onClick={() => navigate("/")}><StyledGoogleLogo/>구글로 시작하기</SocialLoginButton>
+                        <LoginButton onClick={() => navigate("/")}>로그인</LoginButton>
+                        <SignupText onClick={() => navigate("/signup")}>
+                            회원가입 하기
+                        </SignupText>
+                    </DownWrapper>
+                </RightWrapper>
+            </ContentWrapper>
         </PageWrapper>
     )
 }
@@ -322,76 +229,4 @@ const SignupText = styled.div`
     font-size: 0.85em;
     font-weight: 400;
     cursor: pointer;
-`;
-
-// 태블릿 반응형
-const TabletContentWrapper = styled.div`
-    margin-top: 1em;
-    border-radius: 1em;
-    display: flex;
-    flex-direction: column;
-    width: 40em;
-    height: 36em;
-    background-color: #030303;
-    background-image: url(${BackgroundImg});
-    background-size: 30em;
-    background-position: right -10em top -4em;
-    background-repeat: no-repeat;
-`;
-
-const TabletLeftWrapper = styled(LeftWrapper)`
-    font-size: 2em;
-`;
-
-const TabletRightWrapper = styled.div`
-    padding: 2em 3em 3em 3em;
-    box-sizing: border-box;
-    margin: 3em auto;
-    border-radius: 0.7em;
-    width: 60%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    background-color: #1A1A1A;
-    font-size: 0.8125em;
-`;
-
-const TabletStyledInput = styled(StyledInput)`
-    height: 2.8em;
-`;
-
-// 모바일 반응형
-const MobileContentWrapper = styled.div`
-    margin-top: 1em;
-    border-radius: 1em;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 20em;
-    height: 36em;
-    background-color: #030303;
-    background-image: url(${BackgroundImg});
-    background-size: 28em;
-    background-repeat: no-repeat;
-`;
-
-const MobileLeftWrapper = styled(LeftWrapper)`
-    font-size: 1.5em;
-`;
-
-const MobileRightWrapper = styled(RightWrapper)`
-    padding: 1em 2em;
-    width: 70%;
-    height: 26em;
-    font-size: 0.7em;
-`;
-
-const MobileStyledInput = styled(StyledInput)`
-    height: 2.3em;
-`;
-
-const MobileLoginButton = styled(LoginButton)`
-    // height: 2.8em;
-    // line-height: 2.8em;
 `;
