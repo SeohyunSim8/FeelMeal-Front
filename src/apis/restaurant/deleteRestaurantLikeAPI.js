1f8abb36c4ade07fb0911627ea88fdb184262f89
SeohyunSim <angelsh0805@gachon.ac.kr>
2024-11-23 18:38:08 +0900

    마이페이지

---
+++ src/apis/restaurant/deleteRestaurantLikeAPI.js
@@ -0,0 +1,25 @@
+import { api } from "../API";
+
+export const deleteRestaurantLikeAPI = async (memberIdx, restaurantIdx) => {
+    try {
+        console.log(memberIdx);
+        console.log(restaurantIdx);
+        const response = await api.delete('http://localhost:8090/restaurants/like', {
+            data: {
+                memberIdx,
+                restaurantIdx,
+            },
+        });
+        console.log('전체 응답 :', response);
+        return response;
+        
+    } catch (error) {
+        if (error.response && error.response.status === 400) {
+            console.error('좋아요를 누르지 않은 식당입니다.');
+            throw new Error('좋아요를 누르지 않은 식당입니다.');
+        } else {
+            console.error('API 요청 중 오류 발생:', error);
+            throw error; 
+        }
+    }
+};
--- src/pages/MypagePage.jsx
+++ src/pages/MypagePage.jsx
@@ -1,34 +1,68 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { useLocation } from 'react-router-dom';
-import { getProficiencyListAPI } from '../apis/theme/getProficiencyListAPI'; // api 파일
+// api 파일
+import { getRestaurantLikedListAPI } from '../apis/member/getRestaurantLikedListAPI'; 
+import { patchAddressAPI } from '../apis/member/patchAddressAPI'; 
+import { deleteRestaurantLikeAPI } from '../apis/restaurant/deleteRestaurantLikeAPI'; 
 
 export default function MypagePage() {
-//todo
   // state 관리
-  const [profName, setProfName] = useState([]);
-  const [profDescription, setProfDescription] = useState([]);
-  const [cafeLists, setCafeLists] = useState([]);
-  const [page,] = useState('1');
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const [restaurantLikedList, setRestaurantLikedList] = useState([]);
+  const [address, setAddress] = useState('');
 
   // useLocation
-  const location = useLocation();
-  const {level} = location.state || {};
-
+  const memberIdx = 1;
+  
+  // 좋아요 한 식당 목록 불러오기
   useEffect(() => {
-    const fetchStudies = async () => {
-        try {
-            const response = await getProficiencyListAPI(level, page);
-            console.log('받은 데이터:', response);
-            setProfName(response.profName);  // 레벨
-            setProfDescription(response.profDescription);  // 설명
-            setCafeLists(response.contents);  // 리스트
-        } catch (error) {
-            console.error('카페 목록 데이터를 불러오는 중 오류 발생:', error);
-        }
+    const fetchRestaurantsLikedList = async () => {
+      try {
+        const response = await getRestaurantLikedListAPI(memberIdx);
+        console.log('받은 데이터:', response);
+        setRestaurantLikedList(response);  // 식당 목록 저장
+      } catch (error) {
+        console.error('데이터를 불러오는 중 오류 발생:', error);
+      }
     };
-    fetchStudies();
-  }, [level, page]);
+    fetchRestaurantsLikedList();
+  }, []); // foodCategory 변경 시 다시 호출
+  
+  // 식당 좋아요 취소
+  const handleDislikeClick = async (restaurantIdx) => {
+    try {
+      const response = await deleteRestaurantLikeAPI(memberIdx, restaurantIdx);
+
+      if (response.status === 200) 
+        setModalMessage('좋아요가 성공적으로 취소되었습니다.');
+      
+    } catch (error) {
+      if (error.message === '좋아요를 누르지 않은 식당입니다.') {
+        setModalMessage('좋아요를 누르지 않은 식당입니다.');
+      } else {
+          setModalMessage('좋아요 요청 중 오류가 발생했습니다.');
+      }
+    } finally {
+      setShowModal(true); // 항상 모달을 표시
+    }
+  };
+
+  // 주소 수정
+  const handleAddressClick = async (address) => {
+    try {
+      const response = await patchAddressAPI(memberIdx, address);
+
+      if (response.status === 200) {
+        setModalMessage('주소가 수정되었습니다.');
+      }
+    } catch (error) {
+        setModalMessage('요청 중 오류가 발생했습니다.');
+    } finally {
+      setShowModal(true); // 항상 모달을 표시
+    }
+  };
 
 
   return (
@@ -40,10 +74,11 @@ export default function MypagePage() {
                 <AddressHeader>내 위치 수정하기</AddressHeader>
                 <AddressContent>
                     <AddressText>내 위치 (복정동, 태평동을 포함한 지번주소)</AddressText>
-                    <StyledAddress />
+                    <StyledAddress 
+                      value={address} 
+                      onChange={(e) => setAddress(e.target.value)} />
                 </AddressContent>
-                {/* todo */}
-                <AddressButton onClick={() => navigate("/mypage")}> 
+                <AddressButton onClick={() => handleAddressClick(address)}> 
                     수정
                 </AddressButton>
             </Address>
@@ -59,13 +94,15 @@ export default function MypagePage() {
             </TableHeaderWrapper>
             <Table>
               <tbody>
-                {/* todo */}
-                {cafeLists.map((cafe) => (
-                  <TableRow key={cafe.themeId}>
-                    <TableData>{cafe.cityName}</TableData>
-                    <TableData>{cafe.townName}</TableData>
-                    <TableData>{cafe.pointName}</TableData>
-                    <TableData>{cafe.themeName}</TableData>
+                {restaurantLikedList.map((restaurant) => (
+                  <TableRow key={restaurant.idx}>
+                    <TableData>{restaurant.name}</TableData>
+                    <TableData>{restaurant.address}</TableData>
+                    <TableData>
+                      <Button onClick={() => handleDislikeClick(restaurant.idx)}>
+                        좋아요 취소
+                      </Button>
+                    </TableData>
                   </TableRow>
                 ))}
               </tbody>
@@ -73,6 +110,16 @@ export default function MypagePage() {
           </LikeList>
         </LikeListWrapper>
       </Container>
+
+      {/* 모달 */}
+      {showModal && (
+          <ModalBackground onClick={() => setShowModal(false)}> {/* 모달 닫기 */}
+          <ModalContent onClick={(e) => e.stopPropagation()}> {/* 클릭 이벤트 전파 방지 */}
+              <ModalCloseButton onClick={() => setShowModal(false)}>×</ModalCloseButton>
+              <ModalText>{modalMessage}</ModalText>
+          </ModalContent>
+          </ModalBackground>
+      )}
     </Wrapper>
   )
 }
@@ -227,7 +274,6 @@ const Table = styled.table`
   width: 100%;
   height: 100%;
   border-collapse: collapse;
-  margin-top: 3em;
 `;
 
 const TableRow = styled.tr``;
@@ -248,7 +294,7 @@ const TableHeader1 = styled.div`
   font-size: 1.3em;
   text-align: center;
   border-radius: 0.9615em;
-  width: 35%;
+  width: 47%;
 `;
 
 const TableHeader2 = styled.div`
@@ -259,7 +305,7 @@ const TableHeader2 = styled.div`
   font-size: 1.3em;
   text-align: center;
   border-radius: 0.9615em;
-  width: 50%;
+  width: 35%;
 `;
 
 const TableHeader3 = styled.div`
@@ -270,7 +316,7 @@ const TableHeader3 = styled.div`
   font-size: 1.3em;
   text-align: center;
   border-radius: 0.9615em;
-  width: 15%;
+  width: 26%;
 `;
 
 const TableData = styled.td`
@@ -282,4 +328,73 @@ const TableData = styled.td`
   text-align: center;
   border-bottom: 1px solid #fff;
   height: 2.375em;
+`;
+
+const ModalBackground = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 5em;
+  z-index: 999;
+`;
+
+const ModalContent = styled.div`
+  background: #212121;
+  margin-top: 10em;
+  width: 40em;
+  max-height: 80vh;
+  height: 25%;
+  border-radius: 1em;
+  overflow: hidden;
+  position: relative;
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalCloseButton = styled.div`
+  position: absolute;
+  top: 0.5em;
+  right: 1em;
+  font-size: 3em;
+  font-family: 'esamanru-Bold';
+  color: #fff;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const ModalText = styled.td`    
+  color: #fff;
+  font-family: 'Pretendard-Medium';
+  font-size: 1.5em;
+  text-align: center;
+  width: auto;             /* 필요한 너비만큼 */
+  height: auto;            /* 필요한 높이만큼 */
+`;
+
+const Button = styled.div`
+    border: none;
+    border-radius: 0.5em;
+    width: 100%;
+    height: 3em;
+    line-height: 3em;
+    text-align: center;
+    background-color: #940000;
+    color: white;
+    font-family: 'Pretendard-Medium';
+    font-size: 1em;
+    font-weight: 500;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    &:hover{
+        filter: brightness(80%);
+    }
 `;
